ac284a76f53dd2feafd1cea0426e63cae92df110
Josue Perez <josuepe03@hotmail.com>
2024-09-03 23:13:26 -0600

    UPDATE TEXT

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // function([string1, string2],target id,[color1,color2])
-consoleText(["CertusHack"], "text", ["#8A0026", "#FFFFFF", "#FFD700"]);
+consoleText(["consectetur"], "text", ["#8A0026", "#FFFFFF", "#FFD700"]);
 
 function consoleText(words, id, colors) {
   if (colors === undefined) colors = ["#fff"];
